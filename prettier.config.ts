d6acee437de992f4d3cd754f59bf65d2871f180a
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2025-09-03 12:02:36 +0900

    chore(repo): add formatting and tooling configs; update .gitignore

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+	plugins: [
+		"prettier-plugin-organize-imports",
+		"prettier-plugin-packagejson",
+	],
+};
+
+export default config;
